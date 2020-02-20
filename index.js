7cf0a6bca92ab2ee80b1dd956766d2b5c919ca54
Jakub Bröckl <brockljakub@gmail.com>
2020-02-20 14:41:40 +0100

    Creates index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const { ApolloServer, gql } = require("apollo-server");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling",
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen(process.env.PORT).then(({ url }) => {
+  console.log(`Server is listening on: ${url}`);
+});
